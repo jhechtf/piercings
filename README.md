4997b695943efef505bf9d4bd9f07d21e51a799a
Jim Burbridge <jhechtf@gmail.com>
2023-04-11 12:21:38 -0700

    chore: Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Piercings
+
+My daughter, wife, and I all got our earrings pierced. We have to wait 90 days before we can switch them out, and 
+my daughter is constantly asking her mother and I how many days it has been.
+
+So we made this to give her an exact answer at any point in time.
+
+World's simplest Vue + TS app.
---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+  const piercing_date = new Date('2023-03-23');
+  const now = new Date();
+  const change_date = new Date(piercing_date.getTime() + 1000 * 60 * 60 * 24 * 90);
+
+  const SECOND = 1000;
+  const MINUTE = SECOND * 60;
+  const HOUR = MINUTE * 60;
+  const DAY = 24 * HOUR;
+
+  function calculateDays(from: Date, to: Date): string {
+    let difference = to.getTime() - from.getTime();
+    return `${Math.floor(difference / DAY)} day(s)`;
+  }
+
+</script>
+<template>
+  <h1>Days since we got our piercings</h1>
+  <h2>It has been {{calculateDays(piercing_date, now)}} since we got our piercings</h2>
+  <h2>We can change our earrings in {{calculateDays(now, change_date)}} </h2>
+</template>
+
+<style scoped>
+
+</style>
