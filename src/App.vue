64f22569a50d4f321d0c6835b9c4102b0e4e7539
Jim Burbridge <jhechtf@gmail.com>
2023-06-28 20:09:26 -0700

    feat: Updated date to be mom's piercing date.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
   const piercing_date = new Date('2023-03-23');
   const now = new Date();
-  const change_date = new Date(piercing_date.getTime() + 1000 * 60 * 60 * 24 * 90);
+  const change_date = new Date(piercing_date.getTime() + 1000 * 60 * 60 * 24 * 180);
 
   const SECOND = 1000;
   const MINUTE = SECOND * 60;
@@ -17,7 +17,7 @@
 <template>
   <h1>Days since we got our piercings</h1>
   <h2>It has been {{calculateDays(piercing_date, now)}} since we got our piercings</h2>
-  <h2>We can change our earrings in {{calculateDays(now, change_date)}} </h2>
+  <h2>Mom can change her earrings in {{calculateDays(now, change_date)}} </h2>
 </template>
 
 <style scoped>
